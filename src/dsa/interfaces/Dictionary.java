4aa1a4ec331965a2e6e59ff0cfd4d5c552abb487
lungern <xiii.at.cn@gmail.com>
2018-02-01 14:38:01 +0800

    add dictionary structure

--- src/dsa/interfaces/Dictionary.java
+++ src/dsa/interfaces/Dictionary.java
@@ -5,5 +5,13 @@ public interface Dictionary {
 
     boolean isEmpty();
 
+    Entry find(Object key);
 
+    Iterator findAll(Object key);
+
+    Entry insert(Object key, Object value);
+
+    Entry remove(Object key);
+
+    Iterator entries();
 }
